fbf9f9e464ebbff9defbe672abffb70136a9c498
king-kand <stevenokanda@gmail.com>
2022-10-20 17:37:58 +0300

    errors

--- _printf.c
+++ _printf.c
@@ -1,91 +1,83 @@
 #include "main.h"
-
-void print_buffer(char buffer[], int *buff_ind);
-
+static int (*check_for_specifiers(const char *format))(va_list);
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * _printf - write output to stdout
+ *
+ * @format: character string;
+ *
+ * Return: The number of characters printed exluding null byte
  */
-
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
-
-	int flags, width, precision, size, buff_ind = 0;
-
-	va_list list;
-
-	char buffer[BUFF_SIZE];
+	va_list valist;
+	int (*f)(va_list);
+	unsigned int i = 0, count = 0;
 
 	if (format == NULL)
-
 		return (-1);
 
-	va_start(list, format);
-
-	for (i = 0; format && format[i] != '\0'; i++)
-
+	va_start(valist, format);
+	while (format[i])
 	{
-		if (format[i] != '%')
+		for (; format[i] != '%' && format[i]; i++)
 		{
-			buffer[buff_ind++] = format[i];
-
-			if (buff_ind == BUFF_SIZE)
-
-				print_buffer(buffer, &buff_ind);
-
-			/* write(1, &format[i], 1);*/
-
-			printed_chars++;
+			_putchar(format[i]);
+			count++;
 		}
+		if (!format[i])
+			return (count);
+		f = check_for_specifiers(&format[i + 1]);
+		if (f != NULL)
+		{
+			count += f(valist);
+			i += 2;
+			continue;
+		}
+		if (!format[i + 1])
+			return (-1);
 
+		_putchar(format[i]);
+		count++;
+		if (format[i + 1] == '%')
+			i += 2;
 		else
-
-		{
-			print_buffer(buffer, &buff_ind);
-
-			flags = get_flags(format, &i);
-
-			width = get_width(format, &i, list);
-
-			precision = get_precision(format, &i, list);
-
-			size = get_size(format, &i);
-
-			++i;
-
-			printed = handle_print(format, &i, list, buffer,
-
-				flags, width, precision, size);
-
-			if (printed == -1)
-
-				return (-1);
-
-			printed_chars += printed;
-		}
+			i++;
 	}
-
-	print_buffer(buffer, &buff_ind);
-
-	va_end(list);
-
-	return (printed_chars);
+	va_end(valist);
+	return (count);
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
+ *  check_for_specifiers - checks if there is a valid format specifiesr
+ *  @format: possible format specifier
+ *  Return: pointer to valid function or NULL
  */
-
-void print_buffer(char buffer[], int *buff_ind)
-
+static int (*check_for_specifiers(const char *format))(va_list)
 {
-	if (*buff_ind > 0)
+	unsigned int i;
+	print_t p[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"i", print_i},
+		{"d", print_d},
+		{"u", print_u},
+		{"b", print_b},
+		{"o", print_o},
+		{"x", print_x},
+		{"X", print_X},
+		{"p", print_p},
+		{"S", print_S},
+		{"r", print_r},
+		{"R", print_R},
+		{NULL, NULL}
+	};
 
-		write(1, &buffer[0], *buff_ind);
-
-	*buff_ind = 0;
+	for (i = 0; p[i].t != NULL; i++)
+	{
+		if (*(p[i].t) == *format)
+		{
+			break;
+		}
+	}
+	return (p[i].f);
 }
